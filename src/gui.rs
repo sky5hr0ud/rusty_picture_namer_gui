1c0602e5380d9d93e10c4755f03b9c357eb5d551
sky5hr0ud <46011256+sky5hr0ud@users.noreply.github.com>
2022-05-15 11:42:30 -0700

    added checkbox & selection for alt list of filetypes

--- src/gui.rs
+++ src/gui.rs
@@ -4,16 +4,24 @@ use eframe::egui;
 
 mod picture_namer;
 
+const VERSION: &str = "version 0.0.1";
+const NAME: &str = "rusty picture namer gui ";
+//const WEBSITE: &str = 
+
 #[derive(Default)]
 pub struct PictureNamerGUI {
     dropped_files: Vec<egui::DroppedFile>,
     picked_path: Option<String>,
+    use_default: bool,
+    list_of_filetypes_path: Option<String>,
 }
 
 impl eframe::App for PictureNamerGUI {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Drag-and-drop folders onto the window!");
+            let header = String::from(NAME) + VERSION;
+            ui.heading(header);
+            ui.label("Select a folder");
 
             if ui.button("Open folder...").clicked() {
                 if let Some(path) = rfd::FileDialog::new().pick_folder() {
@@ -23,72 +31,34 @@ impl eframe::App for PictureNamerGUI {
 
             if let Some(picked_path) = &self.picked_path {
                 ui.horizontal(|ui| {
+                    ui.label("Picked folder:");
+                    ui.monospace(picked_path);
+                });
+            }
+
+            ui.label("List of filetypes");
+            ui.horizontal(|ui| {
+                ui.add(egui::Checkbox::new(&mut self.use_default, "Use Alternate List of Filetypes"));
+                if ui.button("Open file...").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_file() {
+                        self.list_of_filetypes_path = Some(path.display().to_string());
+                    }
+                }
+            });
+            
+            if let Some(picked_path) = &self.list_of_filetypes_path {
+                ui.horizontal(|ui| {
                     ui.label("Picked file:");
                     ui.monospace(picked_path);
                 });
-                let filetypes = include_str!("_list_of_filetypes.txt").to_string();
-    			let alt_filetypes = alt_get_filetypes(filetypes);
-                directory_walker(picked_path, alt_filetypes.unwrap());
             }
+            ui.label(format!("{}", !self.use_default));
 
-            // Show dropped files (if any):
-            if !self.dropped_files.is_empty() {
-                ui.group(|ui| {
-                    ui.label("Dropped files:");
 
-                    for file in &self.dropped_files {
-                        let mut info = if let Some(path) = &file.path {
-                            path.display().to_string()
-                        } else if !file.name.is_empty() {
-                            file.name.clone()
-                        } else {
-                            "???".to_owned()
-                        };
-                        if let Some(bytes) = &file.bytes {
-                            info += &format!(" ({} bytes)", bytes.len());
-                        }
-                        ui.label(info);
-                    }
-                });
-            }
+
+            let filetypes = include_str!("_list_of_filetypes.txt").to_string();
+            let alt_filetypes = alt_get_filetypes(filetypes);
+            //directory_walker(Some(&self.picked_path), alt_filetypes.unwrap());
         });
-
-        preview_files_being_dropped(ctx);
-
-        // Collect dropped files:
-        if !ctx.input().raw.dropped_files.is_empty() {
-            self.dropped_files = ctx.input().raw.dropped_files.clone();
-        }
-    }
-}
-
-/// Preview hovering files:
-fn preview_files_being_dropped(ctx: &egui::Context) {
-    use egui::*;
-
-    if !ctx.input().raw.hovered_files.is_empty() {
-        let mut text = "Dropping files:\n".to_owned();
-        for file in &ctx.input().raw.hovered_files {
-            if let Some(path) = &file.path {
-                text += &format!("\n{}", path.display());
-            } else if !file.mime.is_empty() {
-                text += &format!("\n{}", file.mime);
-            } else {
-                text += "\n???";
-            }
-        }
-
-        let painter =
-            ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
-
-        let screen_rect = ctx.input().screen_rect();
-        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
-        painter.text(
-            screen_rect.center(),
-            Align2::CENTER_CENTER,
-            text,
-            TextStyle::Heading.resolve(&ctx.style()),
-            Color32::WHITE,
-        );
     }
 }
